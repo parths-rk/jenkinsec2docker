ec1be97f5bf04542f00d8ebb83678335d75e57c4
Parth <parths.singh2003@gmail.com>
2026-01-15 17:43:46 +0000

    Index file correction

---
+++ app/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Jenkins + Docker is working 🚀');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
